b46dc1fb18d96d7c30794a87b87dff9e398817b1
Alexander Zinin <spore_09@mail.ru>
2019-03-26 23:29:20 +0900

    [WIP] add: simple auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ const pug = require('pug');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    const auth = ctx.get('Authorization');
+    if (!auth || !auth.length) {
+        ctx.set('WWW-Authenticate', 'Basic');
+
+        // should not use `.throw` by design
+        // https://github.com/koajs/koa/issues/1127
+        ctx.status = 401;
+    } else {
+        await next();
+    }
+});
+
 app.use(async (ctx) => {
     ctx.body = pug.renderFile('./views/index.pug');
 });
