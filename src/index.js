abdfb2040f084cd2b9ad19c818776007ecc68941
Alexander Zinin <spore_09@mail.ru>
2019-03-28 02:36:45 +0900

    [WIP] add: body mw, lock worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+const debug = require('debug')('nl20');
+
 const Koa = require('koa');
+const koaBody = require('koa-body');
 const pug = require('pug');
 
+const xautolock = require('./xautolock');
+const Locker = require('./lock');
+
+
+const locker = new Locker('zenity');
 const app = new Koa();
 
+const safeActions = {
+    locknow: xautolock.locknow,
+    unlocknow: xautolock.unlocknow,
+};
+
 app.use(async (ctx, next) => {
+    ctx.set('Cache-Control', 'no-store');
+
     const auth = ctx.get('Authorization');
     if (!auth || !auth.length) {
         ctx.set('WWW-Authenticate', 'Basic');
@@ -16,8 +31,43 @@ app.use(async (ctx, next) => {
     }
 });
 
+app.use(async (ctx, next) => {
+    const { method, url } = ctx;
+    if (method !== 'GET') {
+        return next();
+    }
+
+    if (['/', '/index'].includes(url)) {
+        ctx.body = pug.renderFile('./views/index.pug', {
+            locked: await locker.isLocked(),
+        });
+    } else if (['/error'].includes(url)) {
+        ctx.body = pug.renderFile('./views/error.pug', {
+            error: 'unknown error',
+        });
+    }
+
+    return null;
+});
+
+app.use(koaBody());
+
 app.use(async (ctx) => {
-    ctx.body = pug.renderFile('./views/index.pug');
+    if (!ctx.request.body || !ctx.request.body.action) {
+        ctx.throw(400, 'wrong body');
+    }
+
+    const action = safeActions[ctx.request.body.action];
+    if (!action) {
+        ctx.throw(400, 'wrong action');
+    }
+    try {
+        await action();
+        ctx.body = pug.renderFile('./views/action.pug');
+    } catch (error) {
+        console.error(error);
+        ctx.body = pug.renderFile('./views/error.pug', { error });
+    }
 });
 
 app.listen(3000);
